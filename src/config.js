dee546031d28efeaa9b038f05f9a0f72fd692016
Long Le <longle1882@gmail.com>
2025-05-13 17:08:19 +0700

    second commit

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const img_link = 'https://openweathermap.org/img/wn/';
 
 export const weatherVideos = {
-  Clear: '/clear.mp4',
-  Clouds: '/cloud.mp4',
-  Rain: '/rain.mp4',
-  Snow: '/snow.mp4',
-  Thunderstorm: '/thunder.mp4',
-  Mist: '/mist.mp4',
+  Clear: 'clear.mp4',
+  Clouds: 'cloud.mp4',
+  Rain: 'rain.mp4',
+  Snow: 'snow.mp4',
+  Thunderstorm: 'thunder.mp4',
+  Mist: 'mist.mp4',
 };
 
 export { API_KEY, API_URL, img_link };
